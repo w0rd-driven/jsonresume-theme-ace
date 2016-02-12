21a4314a6f47dfec94d713bdfe0156d3294bd3a9
Jeremy Brayton <jbrayton@rethinkgroup.org>
2016-02-12 18:40:30 -0500

    Added very rudimentary DOM structure that still needs a little work. Let's set the structure up first then we can put things in place.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var fileSystem = require("fs");
-var handlebars = require('handlebars');
-var layouts = require('handlebars-layouts');
+var handlebars = require("handlebars");
+var layouts = require("handlebars-layouts");
 
 function render(resume) {
   // Register helpers
+  handlebars.registerHelper("toLower", function(input) {
+    return input.toLowerCase();
+  });
   handlebars.registerHelper(layouts(handlebars));
   // Register partials
-  handlebars.registerPartial('layout', fileSystem.readFileSync(__dirname + '/layout.hbs', 'utf8'));
+  handlebars.registerPartial("layout", fileSystem.readFileSync(__dirname + "/layout.hbs", "utf8"));
   // Compile template
   var template = fileSystem.readFileSync(__dirname + "/resume.hbs", "utf-8");
-  return handlebars.compile(template)({
-    resume: resume
-  });
+  return handlebars.compile(template)(resume);
 }
 
 module.exports = {
