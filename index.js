99981cba88751ae2c37d936a71384fcc730a7b44
Jeremy Brayton <jbrayton@rethinkgroup.org>
2016-03-03 11:09:42 -0500

    #4 Reading the stylesheet via the filesystem and injecting it internally the way Kendall does it

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ function render(resume) {
   handlebars.registerHelper(layouts(handlebars));
   // Register partials
   handlebars.registerPartial("layout", fileSystem.readFileSync(__dirname + "/layout.hbs", "utf8"));
+  // Use internal stylesheet
+  resume.css = fileSystem.readFileSync(__dirname + "/style.css", "utf-8");
   // Compile template
   var template = fileSystem.readFileSync(__dirname + "/resume.hbs", "utf-8");
   return handlebars.compile(template)(resume);
